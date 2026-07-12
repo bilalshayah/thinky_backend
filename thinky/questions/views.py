f936f6a07842c4b6a04a01c3b0ff7095e3651a6c
raghadsiufi <raghadsi2003@gmail.com>
2026-07-12 19:40:54 +0300

    fixed questions

--- thinky/questions/views.py
+++ thinky/questions/views.py
@@ -3,11 +3,18 @@ from rest_framework.generics import RetrieveUpdateDestroyAPIView
 from rest_framework.generics import ListCreateAPIView
 from .serializers import QuestionSerializer , SkillSerializer
 from .models import Question , Skill
+
 # Create your views here.
 
 class QuestionListCreateView(ListCreateAPIView):
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
+
+    # 🌟 إضافة هذا التعديل الذكي ليدعم رفع الأسئلة دفعة واحدة (Bulk Create)
+    def get_serializer(self, *args, **kwargs):
+        if isinstance(kwargs.get('data'), list):
+            kwargs['many'] = True
+        return super().get_serializer(*args, **kwargs)
 
 class QusetionDetailView(RetrieveUpdateDestroyAPIView):
     queryset = Question.objects.all()
