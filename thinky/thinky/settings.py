06614823fe59db845c9c901e4e8ce189a000ac76
obaly7 <obaly7@gmail.com>
2026-07-22 15:51:25 +0300

    fix

--- thinky/thinky/settings.py
+++ thinky/thinky/settings.py
@@ -9,7 +9,6 @@ import os
 from dotenv import load_dotenv
 import os
 
-# 🌟 تفعيل قراءة ملف .env فوراً عند إقلاع السيرفر
 load_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
 
 BASE_DIR = Path(__file__).resolve().parent.parent
